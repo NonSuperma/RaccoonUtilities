20ecb609cddf5f8f29ee8a90197fda56763cd4f9
tobia <tobiasz.kaczala@gmail.com>
2025-03-20 16:17:55 +0100

    Created FullAlbumCreator.py and updated vidMaker to work when imported

---
+++ FullAlbumCreator.py
@@ -0,0 +1,4 @@
+import subprocess as sp
+import tkinter.filedialog
+from vidMaker import makeVideo
+
--- vidMaker.py
+++ vidMaker.py
@@ -1,30 +1,47 @@
 import subprocess as sp
 import tkinter.filedialog
 
-imageInputPath = tkinter.filedialog.askopenfilename()
-soundInputPaths = tkinter.filedialog.askopenfilenames()
 
-if soundInputPaths == "" or imageInputPath == "":
+class RacoonUtilitiesInputError(Exception):
+	def __init__(self, message):
+		self.message = message
+		super().__init__(self.message)
+
+
+def askExit():
+	input("Press enter to exit...")
 	exit()
 
-if len(soundInputPaths) == 1:
-	soundInputPaths = soundInputPaths[0]
-	name = soundInputPaths[:soundInputPaths.rfind(".")]
-	extension = soundInputPaths[soundInputPaths.rfind("."):]
 
-	sp.run(f'ffmpeg -r 1 -loop 1 -i "{imageInputPath}" -i "{soundInputPaths}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{name}.mp4"', shell=True)
-	sp.run(f'del "{imageInputPath.replace('/', '\\')}"', shell=True)
-	sp.run(f'del "{soundInputPaths.replace('/', '\\')}"', shell=True)
-	exit()
+def makeVideo(image_input_path, sound_input_paths):
+	if image_input_path == "" or sound_input_paths == "":
+		raise RacoonUtilitiesInputError("No input")
+	if len(sound_input_paths) == 1:
+		sound_input_paths = sound_input_paths[0]
+		name = sound_input_paths[:sound_input_paths.rfind(".")]
 
-else:
-	names = []
-	for number in range(0, len(soundInputPaths)):
-		names.append(soundInputPaths[number][:soundInputPaths[number].rfind(".")])
-	print(names)
+		sp.run(f'ffmpeg -r 1 -loop 1 -i "{image_input_path}" -i "{sound_input_paths}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{name}.mp4"', shell=True)
+		sp.run(f'del "{image_input_path.replace('/', '\\')}"', shell=True)
+		sp.run(f'del "{sound_input_paths.replace('/', '\\')}"', shell=True)
 
-	for index in range(len(names)):
-		sp.run(f'ffmpeg -r 1 -loop 1 -i "{imageInputPath}" -i "{soundInputPaths[index]}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{names[index]}.mp4"', shell=True)
-		sp.run(f'del "{soundInputPaths[index].replace('/', '\\')}"', shell=True)
-	sp.run(f'del "{imageInputPath.replace('/', '\\')}"', shell=True)
-	exit()
+	else:
+		names = []
+		for number in range(0, len(sound_input_paths)):
+			names.append(sound_input_paths[number][:sound_input_paths[number].rfind(".")])
+		print(names)
+
+		for index in range(len(names)):
+			sp.run(f'ffmpeg -r 1 -loop 1 -i "{image_input_path}" -i "{sound_input_paths[index]}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{names[index]}.mp4"', shell=True)
+			sp.run(f'del "{sound_input_paths[index].replace('/', '\\')}"', shell=True)
+		sp.run(f'del "{image_input_path.replace('/', '\\')}"', shell=True)
+
+
+if __name__ == "__main__":
+	imageInputPath = tkinter.filedialog.askopenfilename()
+	soundInputPaths = tkinter.filedialog.askopenfilenames()
+
+	try:
+		makeVideo(imageInputPath, soundInputPaths)
+	except RacoonUtilitiesInputError:
+		print("No input")
+		askExit()
