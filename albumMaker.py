2a16cd82f5fe40f9143df54a26948eb839a6246b
tobia <tobiasz.kaczala@gmail.com>
2025-03-31 22:56:51 +0200

    Rewritten to use Racoon.py

--- albumMaker.py
+++ albumMaker.py
@@ -1,92 +1,19 @@
-import subprocess as sp
-from tkinter import filedialog, Tk
-
-
-class RacoonUtilitiesInputLengthError(Exception):
-	def __init__(self, message):
-		self.message = message
-		super().__init__(self.message)
-
-
-class RacoonUtilitiesMissingInputError(Exception):
-	def __init__(self, message):
-		self.message = message
-		super().__init__(self.message)
-
-
-def askExit():
-	input("Press enter to exit...")
-	exit()
-
-
-def winDirPath(message):
-	root = Tk()
-	root.lift()
-	root.withdraw()
-	tempPath = filedialog.askdirectory(title=message, parent=root).replace('/', '\\').strip()
-	return tempPath
-
-
-def winFilePath(message):
-	root = Tk()
-	root.lift()
-	root.withdraw()
-	tempPath = filedialog.askopenfilename(title=message, parent=root).replace('/', '\\').strip()
-	return tempPath
-
-
-def winFilesPath(message):
-	root = Tk()
-	root.lift()
-	root.withdraw()
-	tempPaths = list(filedialog.askopenfilenames(title=message, parent=root))
-	for i in range(len(tempPaths)):
-		tempPaths[i] = tempPaths[i].replace('/', '\\')
-	return tempPaths
-
-
-def makeAlbum(image_input_path, sound_input_paths, final_filename, output_path):
-	if image_input_path == "" or sound_input_paths == "" or final_filename == "":
-		raise RacoonUtilitiesMissingInputError("No input")
-
-	if output_path == '':
-		output_path = sound_input_paths[0][:sound_input_paths[0].rfind("\\")]
-	print(output_path)
-
-	inputPath = ''
-	for i in sound_input_paths:
-		inputPath += f'-i "{i.replace('/', '\\')}" '
-
-	preConcat = ''
-	for i in range(0, len(sound_input_paths)):
-		preConcat += f'[{i}:a]'
-
-	extension = sound_input_paths[0][sound_input_paths[0].rfind('.'):]
-
-	sp.run(
-		f'ffmpeg {inputPath}-filter_complex "{preConcat}concat=n={len(sound_input_paths)}:v=0:a=1" {output_path}\\output{extension}',
-		shell=True)
-	sp.run(f'ren "{output_path}\\output{extension}" "{final_filename + extension}"', shell=True)
-
-	sp.run(
-		f'ffmpeg -r 1 -loop 1 -i "{image_input_path}" -i "{output_path}\\{final_filename + extension}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{output_path}\\{final_filename}.mp4"',
-		shell=True)
-	if f'"{output_path}\\{final_filename}.mp4"' != f'{final_filename + extension}':
-		sp.run(f'del {final_filename + extension}', shell=True)
-
+from Racoon import RacoonUtils as Ru
+from Racoon import RacoonErrors as RuE
 
 
 if __name__ == "__main__":
-	imageInputPath = winFilePath('Pick album cover image')
-	soundInputPaths = winFilesPath('Pick songs')
+	imageInputPath = Ru.winFilePath('Pick album cover image')
+	soundInputPaths = Ru.winFilesPath('Pick songs')
 	finalFilename = input("Final file name: ")
-	outputPath = ''
+	outputPath = None
 	try:
-		makeAlbum(imageInputPath, soundInputPaths, finalFilename, outputPath)
-	except RacoonUtilitiesMissingInputError:
+		vid = Ru(imageInputPath, soundInputPaths)
+		vid.makeAlbum(outputPath, finalFilename)
+	except RuE.RacoonUtilitiesMissingInputError:
 		print("RacoonUtilitiesMissingInputError: No input")
 		askExit()
-	except RacoonUtilitiesInputLengthError:
+	except RuE.RacoonUtilitiesInputLengthError:
 		print("RacoonUtilitiesInputLengthError: Too many audio files selected")
 		askExit()
 	exit()
--- vidMaker.py
+++ vidMaker.py
@@ -1,77 +1,15 @@
 import subprocess as sp
-from tkinter import filedialog, Tk
+from Racoon import RacoonUtils as Ru
+from Racoon import RacoonErrors as RuE
 
 
-class RacoonUtilitiesMissingInputError(Exception):
-	def __init__(self, message):
-		self.message = message
-		super().__init__(self.message)
-
-
-def askExit():
-	input("Press enter to exit...")
-	exit()
-
-
-def winDirPath(message):
-	root = Tk()
-	root.lift()
-	root.withdraw()
-	tempPath = filedialog.askdirectory(title=message, parent=root).replace('/', '\\').strip()
-	return tempPath
-
-
-def winFilePath(message):
-	root = Tk()
-	root.lift()
-	root.withdraw()
-	tempPath = filedialog.askopenfilename(title=message, parent=root).replace('/', '\\').strip()
-	return tempPath
-
-
-def winFilesPath(message):
-	root = Tk()
-	root.lift()
-	root.withdraw()
-	tempPaths = list(filedialog.askopenfilenames(title=message, parent=root))
-	for i in range(len(tempPaths)):
-		tempPaths[i] = tempPaths[i].replace('/', '\\')
-	return tempPaths
-
-
-def makeVideo(image_input_path, sound_input_paths, output_path):
-	if image_input_path == "" or sound_input_paths == "":
-		raise RacoonUtilitiesMissingInputError("No input")
-
-	if output_path == '':
-		output_path = sound_input_paths[0][:sound_input_paths[0].rfind("\\")]
-
-	if len(sound_input_paths) == 1:
-		sound_input_paths = sound_input_paths[0]
-		name = sound_input_paths[sound_input_paths.rfind('\\') + 1:sound_input_paths.rfind('.')]
-
-		sp.run(
-			f'ffmpeg -r 1 -loop 1 -i "{image_input_path}" -i "{sound_input_paths}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{output_path}\\{name}.mp4"',
-			shell=True)
-
-	else:
-		names = []
-		for index in range(0, len(sound_input_paths)):
-			names.append(sound_input_paths[index][:sound_input_paths[index].rfind(".")])
-		print(names)
-
-		for index in range(len(names)):
-			sp.run(
-				f'ffmpeg -r 1 -loop 1 -i "{image_input_path}" -i "{sound_input_paths[index]}" -c:v libx264 -acodec copy -r 1 -shortest -vf format=yuv420p "{output_path}\\{names[index]}.mp4"',
-				shell=True)
-
-
-if __name__ == "__main__":
-	imageInputPath = winFilePath("Album cover")
-	soundInputPaths = winFilesPath("Audio files")
-	outputPath = ''
+if _name__ == "__main__":
+	imageInputPath = Ru.winFilePath("Album cover")
+	soundInputPaths = Ru.winFilesPath("Audio files")
+	outputPath = None
 	try:
-		makeVideo(imageInputPath, soundInputPaths, outputPath)
+		vid = Ru(imageInputPath, soundInputPaths)
+		vid.makeVideo(outputPath)
 		for path in soundInputPaths:
 			sp.run(f'del "{path}"', shell=True)
 		sp.run(f'del "{imageInputPath}"', shell=True)
@@ -79,5 +17,4 @@ if __name__ == "__main__":
 	except RacoonUtilitiesMissingInputError:
 		print("No input!")
 		askExit()
-
-exit()
+	exit()
