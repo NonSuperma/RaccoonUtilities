f1505b9f234a4d860032542b724a0374cdfa23ac
tobia <tobiasz.kaczala@gmail.com>
2025-04-27 22:51:01 +0200

    Added: Opening Downloads if not open Cropping 1920x1080 to 1080x1080

--- ClipboardImageGet.py
+++ ClipboardImageGet.py
@@ -1,13 +1,76 @@
-import os.path
+from tkinter import Tk
 from PIL import ImageGrab
 import subprocess
+import sys
+import ctypes
+import time
+import msvcrt
+import os.path
 
-image = ImageGrab.grabclipboard()
-if isinstance(image, type(None)):
-    print("No image in clipboard nigga!")
-    input("Press enter to exit...")
-    exit()
 
-downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-image.save(f'{downloads_path}\\clipboard.png')
-subprocess.run(f'explorer {downloads_path}')
+def get_media_dimentions(file_path) -> list[str] or None:
+    ffprobeOutput = subprocess.run(f'ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 {file_path}', shell=True, capture_output=True)
+    if ffprobeOutput.returncode != 0:
+        return None
+    else:
+        dimentions = ffprobeOutput.stdout.decode().strip().split('x')
+        dimentions = [int(dimention) for dimention in dimentions]
+    return dimentions
+
+def count_open_explorer_downloads_windows():
+    import pygetwindow as gw
+    windows = gw.getWindowsWithTitle('')
+    explorer_windows = [window for window in windows if 'Downloads' in window.title]
+    return len(explorer_windows)
+
+def show_console():
+    root = Tk()
+    root.withdraw()
+    root.lift()
+    ctypes.windll.kernel32.AllocConsole()
+    sys.stdout = open('CONOUT$', 'w')
+    sys.stderr = open('CONOUT$', 'w')
+    sys.stdin = open('CONIN$', 'r')
+
+def hide_console():
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+
+def crop_square_from_1920x1080_media(file_path, output_name) -> None:
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    subprocess.run(f'ffmpeg -i "{file_path}" -vf "crop=1080:1080:420:0" {downloads_path}\\{output_name} -y', shell=True, capture_output=True)
+    return None
+
+def main():
+    hide_console()
+
+    image = ImageGrab.grabclipboard()
+
+    if isinstance(image, type(None)):
+        show_console()
+        print("No image in clipboard nigga!")
+        print("Press any key to exit...")
+
+        start_time = time.time()
+        while True:
+            if msvcrt.kbhit() or time.time() - start_time > 5:
+                break
+            time.sleep(1)
+
+        sys.exit()
+
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    image.save(f'{downloads_path}\\clipboard.png')
+
+    ss_dymentions = get_media_dimentions(f'{downloads_path}\\clipboard.png')
+    if ss_dymentions == [1920, 1080]:
+        crop_square_from_1920x1080_media(f'{downloads_path}\\clipboard.png', f'clipboard__square.png')
+        subprocess.run(f'ffmpeg -i "{downloads_path}\\clipboard__square.png" -vf scale=500:500 "{downloads_path}\\clipboard__square500x500.png" -y', shell=True, capture_output=True)
+
+    if count_open_explorer_downloads_windows() < 1:
+        root = Tk()
+        root.withdraw()
+        root.lift()
+        subprocess.run(f'explorer {downloads_path}')
+
+if __name__ == '__main__':
+    main()
