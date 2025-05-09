8c136877885e7c65ce1faceb4524c9cac6f142e4
tobia <tobiasz.kaczala@gmail.com>
2025-05-10 01:11:58 +0200

    WIP adjusted for changes in Racoon

--- albumMaker.py
+++ albumMaker.py
@@ -2,11 +2,8 @@ import subprocess as sp
 from Racoon import RacoonMediaTools as Ru
 from Racoon import RacoonErrors as RuE
 from playsound3 import playsound
-from tkinter import Tk
+from pathlib import Path
 import sys
-import msvcrt
-import time
-import ctypes
 import os
 
 
@@ -20,39 +17,39 @@ def resource_path(relative_path):
 	return os.path.join(base_path, relative_path)
 
 
-def show_console():
-	root = Tk()
-	root.withdraw()
-	root.lift()
-	ctypes.windll.kernel32.AllocConsole()
-	sys.stdout = open('CONOUT$', 'w')
-	sys.stderr = open('CONOUT$', 'w')
-	sys.stdin = open('CONIN$', 'r')
-
-
-def hide_console():
-	ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
-
 
 if __name__ == "__main__":
-	imageInputPath = Ru.winFilePath('Pick the album cover image')
-	soundInputPaths = Ru.winFilesPath('Pick songs')
+	test = True
+	if not test:
+		imageInputPath = Ru.winFilePath('Pick the album cover image')
+	else:
+		imageInputPath = Path('C:\\Users\\tobia\\Downloads\\New folder\\cover.jpg')
+
+	if test:
+		soundInputPaths = [Path('C:/Users/tobia/Downloads/New folder/Stoned Jesus - Electric Mistress.opus'), Path("C:/Users/tobia/Downloads/New folder/Stoned Jesus - I'm The Mountain.opus"), Path('C:/Users/tobia/Downloads/New folder/Stoned Jesus - Indian.opus')]
+	else:
+		soundInputPaths = Ru.winFilesPath('Pick songs')
 
 	workingFolderPath = soundInputPaths[0].parent
 	workingFolderName = workingFolderPath.name
-	userChoice = input(
-		f'The selected songs are in a folder called "{workingFolderName}"\nPress enter to use that folder as the album name, or input the album name manually\n: ')
-	if userChoice == '':
-		albumName = workingFolderName
+	if not test:
+		userChoice = input(
+			f'The selected songs are in a folder called "{workingFolderName}"\nPress enter to use that folder as the album name, or input the album name manually\n: ')
+		if userChoice == '':
+			albumName = workingFolderName
+		else:
+			albumName = userChoice
 	else:
-		albumName = userChoice
+		albumName = 'TEST'
 
 	outputPath = None
 
-	vid = Ru(imageInputPath, soundInputPaths)
-	vid.makeAlbum(outputPath, albumName)
+	album = Ru(imageInputPath, soundInputPaths)
+	album.makeAlbum(outputPath, albumName)
 
-	#sound_file = resource_path('au5-1.mp3')
-	#playsound(sound_file)
+
+
+	sound_file = resource_path('au5-1.mp3')
+	playsound(sound_file)
 
 	sys.exit(0)
