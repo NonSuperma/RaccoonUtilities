fb7b677603b779d18f078efd7390aa0253bd6d28
tobia <tobiasz.kaczala@gmail.com>
2025-05-13 03:54:52 +0200

    Improved readability

--- albumMaker.py
+++ albumMaker.py
@@ -1,8 +1,9 @@
-import subprocess as sp
+from colorama import init, Fore, Back, Style
 from Racoon import RacoonMediaTools as Ru
 from Racoon import RacoonErrors as RuE
 from playsound3 import playsound
 from pathlib import Path
+import subprocess as sp
 import sys
 import os
 
@@ -20,36 +21,39 @@ def resource_path(relative_path):
 
 if __name__ == "__main__":
 	test = False
-	if not test:
-		imageInputPath = Ru.winFilePath('Pick the album cover image')
+	if test:
+		imageInputPath = Path('Test_Source\\clipboard__square.png')
 	else:
-		imageInputPath = Path('C:\\Users\\tobia\\Downloads\\New folder\\cover.jpg')
+		imageInputPath = Ru.winFilePath('Pick the album cover image')
 
 	if test:
-		soundInputPaths = [Path('C:/Users/tobia/Downloads/New folder/Stoned Jesus - Electric Mistress.opus'), Path("C:/Users/tobia/Downloads/New folder/Stoned Jesus - I'm The Mountain.opus"), Path('C:/Users/tobia/Downloads/New folder/Stoned Jesus - Indian.opus')]
+		soundInputPaths = [file for file in p.rglob("*.webp")]
 	else:
 		soundInputPaths = Ru.winFilesPath('Pick songs')
 
 	workingFolderPath = soundInputPaths[0].parent
 	workingFolderName = workingFolderPath.name
-	if not test:
+
+	if test:
+		albumName = 'TEST'
+	else:
 		userChoice = input(
-			f'The selected songs are in a folder called "{workingFolderName}"\nPress enter to use that folder as the album name, or input the album name manually\n: ')
+			f'{Fore.CYAN}[Info]{Fore.RESET} The selected songs are in a folder called {Fore.GREEN}"{workingFolderName}"{Fore.RESET}\n'
+			f'Press {Back.BLACK}ENTER{Back.RESET} to use that folder as the album name, or input the album name manually\n: '
+		)
+
 		if userChoice == '':
 			albumName = workingFolderName
+
 		else:
 			albumName = userChoice
-	else:
-		albumName = 'TEST'
 
 	outputPath = None
 
 	album = Ru(imageInputPath, soundInputPaths)
 	album.makeAlbum(outputPath, albumName)
 
-
-
-	sound_file = resource_path('au5-1.mp3')
+	sound_file = str(resource_path('au5-1.mp3'))
 	playsound(sound_file)
 
 	sys.exit(0)
