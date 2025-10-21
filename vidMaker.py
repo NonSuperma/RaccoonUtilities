30f3794d8f3e1386369ba2eb6ea3d329e126fa71
tobia <tobiasz.kaczala@gmail.com>
2025-10-21 07:42:46 +0200

    redid the whole thing idk

--- vidMaker.py
+++ vidMaker.py
@@ -12,138 +12,139 @@ import sys
 import pyperclip
 import os
 
+
 def main():
-	global folderPath, mp4FinalPath
-	try:
-		flacFilePaths = winFilesPath('Audio', 'audio')
-	except MissingInputError:
-		askExit(f'{Fore.RED}Audio selection window closed.')
+    global folderPath, mp4FinalPath
+    try:
+        flacFilePaths = winFilesPath('Audio', 'audio')
+    except MissingInputError:
+        askExit(f'{Fore.RED}Audio selection window closed.')
 
-	folderPath = flacFilePaths[0].parent
+    folderPath = flacFilePaths[0].parent
 
-	label = ''
+    label = ''
 
-	temp = str(folderPath).split('\\')
-	for index in range(len(temp)):
-		temp[index] = temp[index].replace("'", '').strip()
-	folderPath_safe = Path('\\'.join(temp))
-	trackCount = len(flacFilePaths)
+    temp = str(folderPath).split('\\')
+    for index in range(len(temp)):
+        temp[index] = temp[index].replace("'", '').strip()
+    folderPath_safe = Path('\\'.join(temp))
+    trackCount = len(flacFilePaths)
 
-	flacFilePaths_safe = []
-	for path in flacFilePaths:
-		flacFilePaths_safe.append(Path.joinpath(folderPath_safe, str(path.name).replace("'", '')))
+    flacFilePaths_safe = []
+    for path in flacFilePaths:
+        flacFilePaths_safe.append(Path.joinpath(folderPath_safe, str(path.name).replace("'", '')))
 
-	for index in range(trackCount):
-		flacFilePaths[index].rename(Path.joinpath(folderPath, flacFilePaths_safe[index].name))
+    for index in range(trackCount):
+        flacFilePaths[index].rename(Path.joinpath(folderPath, flacFilePaths_safe[index].name))
 
-	folderPath.rename(Path.joinpath(folderPath.parent, folderPath_safe.name))
-	# folderPath.parent.rename(Path.joinpath(folderPath.parent.parent, folderPath_safe.parent.name))
+    folderPath.rename(Path.joinpath(folderPath.parent, folderPath_safe.name))
+    # folderPath.parent.rename(Path.joinpath(folderPath.parent.parent, folderPath_safe.parent.name))
 
-	try:
-		coverPath = winFilePath('Cover', 'image', initialDir=folderPath_safe)
-	except MissingInputError:
+    try:
+        coverPath = winFilePath('Cover', 'image', initialDir=folderPath_safe)
+    except MissingInputError:
 
-		for index in range(trackCount):
-			flacFilePaths_safe[index].rename(Path.joinpath(folderPath_safe, flacFilePaths[index].name))
+        for index in range(trackCount):
+            flacFilePaths_safe[index].rename(Path.joinpath(folderPath_safe, flacFilePaths[index].name))
 
-		folderPath_safe.rename(Path.joinpath(folderPath_safe.parent, folderPath.name))
+        folderPath_safe.rename(Path.joinpath(folderPath_safe.parent, folderPath.name))
 
-		askExit(f'{Fore.RED}Cover image selection window closed.')
+        askExit(f'{Fore.RED}Cover image selection window closed.')
 
-	coverDimentions = ScaleToEven(coverPath).dimensions
-	if coverDimentions[0] > 1000 and coverDimentions[1] > 1000 and (coverDimentions != [1000, 1000]):
-		coverPath = scale_image(coverPath, '1000:1000', remove_old=False)
-		# userChoice = input(f'Scale album cover to 1000x1000? Enter=Yes, n=No\n'
-		# 				   f': ')
-		# if userChoice != 'n':
-		# 	print(f'Converting cover...')
-		# 	coverPath = ScaleImage(coverPath, '1000:1000', remove_old=False)
-		# 	print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
+    coverDimentions = ScaleToEven(coverPath).dimensions
+    if coverDimentions[0] > 1000 and coverDimentions[1] > 1000 and (coverDimentions != [1000, 1000]):
+        coverPath = scale_image(coverPath, '1000:1000', remove_old=False)
+    # userChoice = input(f'Scale album cover to 1000x1000? Enter=Yes, n=No\n'
+    # 				   f': ')
+    # if userChoice != 'n':
+    # 	print(f'Converting cover...')
+    # 	coverPath = ScaleImage(coverPath, '1000:1000', remove_old=False)
+    # 	print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
 
-	if trackCount > 1:
-		with open(f'{folderPath_safe}\\temp_audio_list.txt', 'w+', encoding='utf-8') as f:
-			for file in flacFilePaths_safe:
-				f.write(f"file '{str(file)}'\n")
+    if trackCount > 1:
+        with open(f'{folderPath_safe}\\temp_audio_list.txt', 'w+', encoding='utf-8') as f:
+            for file in flacFilePaths_safe:
+                f.write(f"file '{str(file)}'\n")
 
-		print(f'Concading "{folderPath_safe.name}.flac"...')
-		concadedFlacPath = Path.joinpath(folderPath_safe, str(folderPath_safe.name) + '.flac')
-		ffmpegOutput_concad = subprocess.run(
-			f'ffmpeg '
-			f'-y '
-			f'-loglevel fatal '
-			f'-f concat '
-			f'-safe 0 '
-			f'-i "{folderPath_safe}\\temp_audio_list.txt" '
-			f'"{concadedFlacPath}"'
-		)
-		print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
-		Path(f"{folderPath_safe}\\temp_audio_list.txt").unlink()
-	else:
-		concadedFlacPath = Path.joinpath(folderPath_safe, str(flacFilePaths_safe[0].name))
+        print(f'Concading "{folderPath_safe.name}.flac"...')
+        concadedFlacPath = Path.joinpath(folderPath_safe, str(folderPath_safe.name) + '.flac')
+        ffmpegOutput_concad = subprocess.run(
+            f'ffmpeg '
+            f'-y '
+            f'-loglevel fatal '
+            f'-f concat '
+            f'-safe 0 '
+            f'-i "{folderPath_safe}\\temp_audio_list.txt" '
+            f'"{concadedFlacPath}"'
+        )
+        print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
+        Path(f"{folderPath_safe}\\temp_audio_list.txt").unlink()
+    else:
+        concadedFlacPath = Path.joinpath(folderPath_safe, str(flacFilePaths_safe[0].name))
 
-	print(f'Converting {concadedFlacPath.name} to mp3')
-	mp3FilePath = Path.joinpath(folderPath_safe, f'{concadedFlacPath.stem}.mp3')
-	subprocess.run(
-		f'ffmpeg '
-		f'-y '
-		f'-loglevel fatal '
-		f'-i "{concadedFlacPath}" '
-		f'-map_metadata 0 '
-		f'-id3v2_version 3 -write_id3v1 1 '
-		f'-c:a libmp3lame -b:a 320k '
-		f'"{mp3FilePath}"'
-	)
-	print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
+    print(f'Converting {concadedFlacPath.name} to mp3')
+    mp3FilePath = Path.joinpath(folderPath_safe, f'{concadedFlacPath.stem}.mp3')
+    subprocess.run(
+        f'ffmpeg '
+        f'-y '
+        f'-loglevel fatal '
+        f'-i "{concadedFlacPath}" '
+        f'-map_metadata 0 '
+        f'-id3v2_version 3 -write_id3v1 1 '
+        f'-c:a libmp3lame -b:a 320k '
+        f'"{mp3FilePath}"'
+    )
+    print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
 
-	print(f'Converting "{mp3FilePath.name}" to mp4...')
-	if len(flacFilePaths) > 1:
-		label += ' [Full Album]'
-	artist = str(mp3FilePath.parent.parent.name)
-	if artist not in mp3FilePath.name:
-		mp4FinalPath = Path.joinpath(folderPath_safe, artist + ' - ' + str(mp3FilePath.stem) + label + '.mp4')
-	else:
-		mp4FinalPath = Path.joinpath(folderPath_safe, str(mp3FilePath.stem) + label + '.mp4')
-	subprocess.run(
-		f'ffmpeg '
-		f'-loglevel fatal '
-		f'-y '
-		f'-loop 1 '
-		f'-framerate 1 '
-		f'-i "{coverPath}" '
-		f'-i "{mp3FilePath}" '
-		f'-map 0:v:0 -map 1:a:0 '
-		f'-c:v libx264 -crf 30 -tune stillimage '
-		f'-c:a copy '
-		f'-shortest '
-		f'-movflags +faststart '
-		f'-map_metadata 1 '
-		f'-id3v2_version 3 -write_id3v1 1 '
-		f'-vf "format=yuv420p" '
-		f'-r 1 '
-		f'"{mp4FinalPath}"'
-	)
-	print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
+    print(f'Converting "{mp3FilePath.name}" to mp4...')
+    if len(flacFilePaths) > 1:
+        label += ' [Full Album]'
+    artist = str(mp3FilePath.parent.parent.name)
+    if artist not in mp3FilePath.name:
+        mp4FinalPath = Path.joinpath(folderPath_safe, artist + ' - ' + str(mp3FilePath.stem) + label + '.mp4')
+    else:
+        mp4FinalPath = Path.joinpath(folderPath_safe, str(mp3FilePath.stem) + label + '.mp4')
+    subprocess.run(
+        f'ffmpeg '
+        f'-loglevel fatal '
+        f'-y '
+        f'-loop 1 '
+        f'-framerate 1 '
+        f'-i "{coverPath}" '
+        f'-i "{mp3FilePath}" '
+        f'-map 0:v:0 -map 1:a:0 '
+        f'-c:v libx264 -crf 30 -tune stillimage '
+        f'-c:a copy '
+        f'-shortest '
+        f'-movflags +faststart '
+        f'-map_metadata 1 '
+        f'-id3v2_version 3 -write_id3v1 1 '
+        f'-vf "format=yuv420p" '
+        f'-r 1 '
+        f'"{mp4FinalPath}"'
+    )
+    print(f'{Fore.LIGHTGREEN_EX}Done!{Fore.RESET}\n')
 
-	if trackCount > 1:
-		concadedFlacPath.unlink()
-	mp3FilePath.unlink()
+    if trackCount > 1:
+        concadedFlacPath.unlink()
+    mp3FilePath.unlink()
 
-	for index in range(trackCount):
-		flacFilePaths_safe[index].rename(Path.joinpath(folderPath_safe, flacFilePaths[index].name))
+    for index in range(trackCount):
+        flacFilePaths_safe[index].rename(Path.joinpath(folderPath_safe, flacFilePaths[index].name))
 
-	folderPath_safe.rename(Path.joinpath(folderPath_safe.parent, folderPath.name))
+    folderPath_safe.rename(Path.joinpath(folderPath_safe.parent, folderPath.name))
 
 
 if __name__ == "__main__":
-	global mp4FinalPath
-	global folderPath
-	try:
-		colorama.init(autoreset=True)
-		main()
-		playsound(resource_path('au5-1.mp3'))
-		os.startfile(folderPath)
-		pyperclip.copy(str(mp4FinalPath.name))
-		sys.exit(0)
-	except Exception as e:
-		print(f'{Fore.RED}{str(e)}{Fore.RESET}')
-		input(f'{Fore.WHITE}Press any key to exit.')
+    global mp4FinalPath
+    global folderPath
+    try:
+        colorama.init(autoreset=True)
+        main()
+        playsound(resource_path('au5-1.mp3'))
+        os.startfile(folderPath)
+        pyperclip.copy(str(mp4FinalPath.name))
+        sys.exit(0)
+    except Exception as e:
+        print(f'{Fore.RED}{str(e)}{Fore.RESET}')
+        input(f'{Fore.WHITE}Press any key to exit.')
